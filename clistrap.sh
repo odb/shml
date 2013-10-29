0f0d6dfd075d1b52d4c1eec3f7944778576abf5c
Josh Mervine <jmervine@yp.com>
2013-10-28 17:06:08 -0700

    Saving work in progress, not working ATM.

--- clistrap.sh
+++ clistrap.sh
@@ -3,13 +3,13 @@
 #************************************************#
 #    Clistrap - Style Framework for The Terminal
 #                    Beta
-#        by Justin Dorfman - @jdorfman            
+#        by Justin Dorfman - @jdorfman
 #                10-27-2013
 #
-#               Inspired by: 
-#       Kiyor Cia, Jeff Foard FLOZz' MISC, 
+#               Inspired by:
+#       Kiyor Cia, Jeff Foard FLOZz' MISC,
 #  Joshua Mervine, Mark Otto & Dave Gandy
-# 
+#
 #
 #     https://github.com/jdorfman/sTre-lang
 #************************************************#
@@ -17,94 +17,94 @@
 ## Foreground (Text)
 # cs == clistrap
 # f  == Foreground
-# l  == Light
-# d  == Dark
-# e  == End
 
 ## Adjectives
 
-cs_e_f='\033[39m' #End Foreground Color
-cs_f_black='\033[30m'
-cs_f_red='\033[31m'
-cs_f_green='\033[32m'
-cs_f_yellow='\033[33m'
-cs_f_blue='\033[34m'
-cs_f_magenta='\033[35m'
-cs_f_cyan='\033[36m'
-cs_f_l_gray='\033[90m'
-cs_f_d_gray='\033[91m'
-cs_f_l_green='\033[92m'
-cs_f_l_yellow='\033[93m'
-cs_f_l_blue='\033[94m'
-cs_f_l_magenta='\033[95m'
-cs_f_l_cyan='\033[96m'
-cs_f_white='\033[97m'
+cs_f[end]='\033[39m' #End Foreground Color
+cs_f[black]='\033[30m'
+cs_f[red]='\033[31m'
+cs_f[green]='\033[32m'
+cs_f[yellow]='\033[33m'
+cs_f[blue]='\033[34m'
+cs_f[magenta]='\033[35m'
+cs_f[cyan]='\033[36m'
+cs_f[lightgray]='\033[90m'
+cs_f[darkgray]='\033[91m'
+cs_f[lightgreen]='\033[92m'
+cs_f[lightyellow]='\033[93m'
+cs_f[lightblue]='\033[94m'
+cs_f[lightmagenta]='\033[95m'
+cs_f[lightcyan]='\033[96m'
+cs_f[white]='\033[97m'
 
 # HEX
-
-cs_f_000000='\033[30m'
-cs_f_F00BAF='\033[31m'
-cs_f_00CD00='\033[32m'
-cs_f_CDCD00='\033[33m'
-cs_f_0286fe='\033[34m'
-cs_f_e100cc='\033[35m'
-cs_f_00d3cf='\033[36m'
-cs_f_e4e4e4='\033[90m'
-cs_f_4c4c4c='\033[91m'
-cs_f_00fe00='\033[92m'
-cs_f_f8fe00='\033[93m'
-cs_f_3a80b5='\033[94m'
-cs_f_fe00fe='\033[95m'
-cs_f_00fefe='\033[96m'
-cs_f_ffffff='\033[97m'
+# todo make function with switch
+#
+# function cs_f[hex]() { }
+#
+#cs_f[000000]='\033[30m'
+#cs_f[F00BAF]='\033[31m'
+#cs_f[00CD00]='\033[32m'
+#cs_f[CDCD00]='\033[33m'
+#cs_f[0286fe]='\033[34m'
+#cs_f[e100cc]='\033[35m'
+#cs_f[00d3cf]='\033[36m'
+#cs_f[e4e4e4]='\033[90m'
+#cs_f[4c4c4c]='\033[91m'
+#cs_f[00fe00]='\033[92m'
+#cs_f[f8fe00]='\033[93m'
+#cs_f[3a80b5]='\033[94m'
+#cs_f[fe00fe]='\033[95m'
+#cs_f[00fefe]='\033[96m'
+#cs_f[ffffff]='\033[97m'
 
 ## Background
 # cs == Clistrap
 # b  == Background
-# l  == Light
-# d  == Dark
-# e  == End
 
-cs_e_b='\033[49m' #End Background Color
-cs_b_black='\033[40m'
-cs_b_red='\033[41m'
-cs_b_green='\033[42m'
-cs_b_yellow='\033[43m'
-cs_b_blue='\033[144m'
-cs_b_magenta='\033[45m'
-cs_b_cyan='\033[46m'
-cs_b_l_gray='\033[47m'
-cs_b_d_gray='\033[100m'
-cs_b_l_red='\033[101m'
-cs_b_l_green='\033[102m'
-cs_b_l_yellow='\033[103m'
-cs_b_l_blue='\033[104m'
-cs_b_l_magenta='\033[105m'
-cs_b_l_cyan='\033[106m'
-cs_b_white='\033[107m'
+cs_b[end]='\033[49m' #End Background Color
+cs_b[black]='\033[40m'
+cs_b[red]='\033[41m'
+cs_b[green]='\033[42m'
+cs_b[yellow]='\033[43m'
+cs_b[blue]='\033[144m'
+cs_b[magenta]='\033[45m'
+cs_b[cyan]='\033[46m'
+cs_b[lightgray]='\033[47m'
+cs_b[darkgray]='\033[100m'
+cs_b[lightred]='\033[101m'
+cs_b[lightgreen]='\033[102m'
+cs_b[lightyellow]='\033[103m'
+cs_b[lightblue]='\033[104m'
+cs_b[lightmagenta]='\033[105m'
+cs_b[lightcyan]='\033[106m'
+cs_b[white]='\033[107m'
 
 ## Gradients
 # g == Gradient
 
-cs_g_rainbow="`for i in {16..21}{21..16}; do echo -en "\033[48;5;${i}m \033[0m"; done; echo`"
-cs_g_yellow="`for i in {226..229}{229..226}; do echo -en "\033[48;5;${i}m \033[0m"; done; echo`"
+cs_g[rainbow]="`for i in {16..21}{21..16}; do echo -en '\033[48;5;${i}m \033[0m'; done; echo`"
+cs_g[yellow]="`for i in {226..229}{229..226}; do echo -en '\033[48;5;${i}m \033[0m'; done; echo`"
 
 ## Attributes
 
-cs_e_a='\033[0m' # End Attribute Style
-cs_reset='\033[0m'
-cs_bold='\033[1m'
-cs_dim='\033[2m'
-cs_italic='\033[4m'
-cs_blink='\033[5m'
-cs_invert='\033[7m'
-cs_hidden='\033[8m'
+cs_a[end]='\033[0m' # End Attribute Style
+cs_a[reset]='\033[0m'
+cs_a[bold]='\033[1m'
+cs_a[dim]='\033[2m'
+cs_a[italic]='\033[4m'
+cs_a[blink]='\033[5m'
+cs_a[invert]='\033[7m'
+cs_a[hidden]='\033[8m'
 
 ## Elements
 
 cs_br='\n'
 cs_hr='---------------------------------'
-cs_hr_stars='*********************************'
+cs_hr[star]='*********************************'
+cs_hr[pound]='#################################'
+cs_hr[tilde]='~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+cs_hr[dot]='.................................'
 
 ## Animations
 
@@ -112,7 +112,7 @@ cs_hr_stars='*********************************'
 cs_loadingbar='bar -c'
 
 # http://fitnr.com/showing-a-bash-spinner.html
-spinner()
+cs_spinner()
 {
     local pid=$1
     local delay=0.75
@@ -146,3 +146,4 @@ cs_copyright='©'
 cs_apple=''
 cs_smile_face="☺"
 
+# vim: ft=sh:
--- example.sh
+++ example.sh
@@ -3,19 +3,18 @@ source clistrap.sh
 ## Examples
 
 fake_install_yes() {
-	
-	echo -e "\nCreating Coversheet for TPS Reports... ${cs_f_green}${cs_checkmark} Pass${cs_e_f}"
+	echo -e "\nCreating Coversheet for TPS Reports... ${cs_f[green]}${cs_checkmark} Pass${cs_f[end]}"
 	sleep 1;
-	echo -e "Misplaced decimal check... ${cs_f_red}${cs_xmark} Fail${cs_e_f}"
+	echo -e "Misplaced decimal check... ${cs_f[red]}${cs_xmark} Fail${cs_f[end]}"
 	sleep 1;
-	echo -e "Divert fractions of pennies into a bank account... ${cs_f_green}${cs_checkmark} Pass${cs_e_f}"
+	echo -e "Divert fractions of pennies into a bank account... ${cs_f[green]}${cs_checkmark} Pass${cs_f[end]}"
 	sleep 1;
-	echo -e "${cs_f_green}${cs_checkmark} Program installed${cs_e_f}"
+	echo -e "${cs_f[green]}${cs_checkmark} Program installed${cs_f[end]}"
 	sleep 1;
-	echo -e "Account Balance: ${cs_bold}${cs_f_red}\$305,326.13 ${cs_e_f}${cs_e_a}"
+	echo -e "Account Balance: ${cs_a[bold]}${cs_f[red]}\$305,326.13 ${cs_f[end]}${cs_a[end]}"
 	sleep 1;
 	echo -e "${cs_hr}${cs_br}"
-	echo -e "${cs_bold}${cs_f_red}CALL MICHAEL! ${cs_e_f}${cs_e_a}"
+	echo -e "${cs_a[bold]}${cs_f[red]}CALL MICHAEL! ${cs_f[end]}${cs_a[end]}"
 }
 
 while true; do
@@ -23,28 +22,28 @@ while true; do
     case $yn in
         [Yy]* ) fake_install_yes; echo -e ${cs_br}; sleep 5; break;;
         [Nn]* ) exit;;
-        * ) echo -e "Please answer ${cs_b_green}yes${cs_e_b} or ${cs_b_red}no${cs_e_b}.";;
+        * ) echo -e "Please answer ${cs_b[green]}yes${cs_b[end]} or ${cs_b[red]}no${cs_b[end]}.";;
     esac
 done
 
 #echo -e "${cs_f_red}It's Spinning: `spinner $! &`${cs_e_f}"
 
 # echo -e "${cs_br}";
-# 
+#
 # echo -e " ${cs_f_red}${cs_heart} The Red Heart${cs_e_f}";
 # echo -e " ${cs_f_green}${cs_apple} The Green Apple${cs_e_f}";
 # echo -e " ${cs_f_CDCD00}${cs_sun} The Yellow Sun${cs_e_f}";
 # echo -e " ${cs_f_l_blue}${cs_star} The Light Blue Star${cs_e_f}";
 # echo -e " ${cs_f_white}${cs_music} See the music?${cs_e_f}";
-# 
-# 
+#
+#
 # echo -e "${cs_br}";
-# 
+#
 # echo -e "${cs_f_green}${cs_checkmark} Pass${cs_e_f}";
 # echo -e "${cs_f_red}${cs_xmark} Fail${cs_e_f}";
 # echo -e "${cs_br}";
-# 
-# 
+#
+#
 # echo -e "${cs_f_green}Green Text${cs_e_f}";
 # echo -e "${cs_f_yellow}Yellow Text${cs_e_f}";
 # echo -e "${cs_f_blue}Blue Text${cs_e_f}";
@@ -57,3 +56,5 @@ done
 # echo -e "${cs_br}";
 # echo -e "Reading Rainbow: ${cs_g_rainbow}${cs_e_b}";
 # echo -e "Yellow Gradient: ${cs_g_yellow}${cs_e_b}";
+
+# vim: ft=sh:

7c4997ceef96caf1147340f1078b875cc7efd73e
Justin Dorfman <justindorfman@gmail.com>
2017-02-11 10:38:20 -0800

    Updated social buttons

---
+++ docs/_posts/_site/public/code/foreground.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source "$(which shml)"
+
+echo "$(color green "RZA GZA") $(color end)"
+echo "
+$(color yellow)
+Inspectah Deck
+Ghostface Killah
+$(color end)
+"
+echo "$(c red "Killa Beez on a swarm") $(c end)"
